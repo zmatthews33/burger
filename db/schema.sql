d9f350b62d20da66b8bb208b0befb7267784a0b8
whatevermane <zmatt33@gmail.com>
2019-04-30 13:25:29 -0500

    edited burger names

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,12 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
-CREATE TABLE burgers(
-    id INTEGER NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(30),
+CREATE TABLE burgers
+(
+    id INTEGER NOT NULL
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (150),
     devoured BOOLEAN,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+    (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
 INSERT INTO burgers
     (burger_name,devoured)
-VALUES("Double Bacon Cheeseburger", false);
+VALUES("Grandpa Muenster Burger", false);
 INSERT INTO burgers
     (burger_name,devoured)
-VALUES("Black Jack", false);
+VALUES("Say It Ain't Cilantro Burger (Doesn’t come with cilantro… because cilantro is terrible)", false);
 INSERT INTO burgers
     (burger_name,devoured)
-VALUES("Peanut Butter Bacon", false);
+VALUES("Chorizo Your Own Adventure Burger", false);
