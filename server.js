57c2b8dfb7623bca3f82d7a3402d3e180f3a05ee
whatevermane <zmatt33@gmail.com>
2019-05-01 16:03:09 -0500

    Editing JAWSDB_URL

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = process.env.PORT || 3000;
 
@@ -6,20 +6,17 @@ const app = express();
 
 app.use(express.static("public"));
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require('./controllers/burgers_Controller.js')
-app.use(routes)
-
+const routes = require("controllers/burgers_Controller.js");
+app.use(routes);
 
 app.listen(PORT, () => {
-    console.log("Server is listening on http://localhost:" + PORT)
-})
+  console.log("Server is listening on http://localhost:" + PORT);
+});
