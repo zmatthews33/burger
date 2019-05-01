e963a227ad226fe2156369a9bb199ec9feb15f52
whatevermane <zmatt33@gmail.com>
2019-05-01 16:08:59 -0500

    JAWS edits

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./burgers_controller.js");
+const routes = require("./controllers/burgers_controller");
 app.use(routes);
 
 app.listen(PORT, () => {
